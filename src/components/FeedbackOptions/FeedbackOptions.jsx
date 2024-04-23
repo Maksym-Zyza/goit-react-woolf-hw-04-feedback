a014e69f9c04bc73a84c41c4e4a00dd261285afb
Maksym-Zyza <mmziza88@gmail.com>
2024-04-23 21:17:08 +0300

    updated FeedbackOptions, Statistics

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,25 +1,21 @@
-import { Component } from 'react';
 import css from './FeedbackOptions.module.scss';
 
-class FeedbackOptions extends Component {
-  render() {
-    const { options, onLeaveFeedback } = this.props;
-    const toLowerCase = option => option.toLowerCase();
-    return (
-      <ul className={css.feedbackOptions}>
-        {options.map(option => (
-          <li key={option}>
-            <button
-              onClick={() => onLeaveFeedback(toLowerCase(option))}
-              name={toLowerCase(option)}
-            >
-              {option}
-            </button>
-          </li>
-        ))}
-      </ul>
-    );
-  }
-}
+const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  const toLowerCase = option => option.toLowerCase();
+  return (
+    <ul className={css.feedbackOptions}>
+      {options.map(option => (
+        <li key={option}>
+          <button
+            onClick={() => onLeaveFeedback(toLowerCase(option))}
+            name={toLowerCase(option)}
+          >
+            {option}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export default FeedbackOptions;
